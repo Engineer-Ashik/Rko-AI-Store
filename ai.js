22637d6f9b154c9d5562a19a2aab8cc2e7c9f71d
Ashik <engineerashikur@gmail.com>
2024-11-26 23:22:23 +0600

    Added searchbar and button and  a card

--- ai.js
+++ ai.js
@@ -1,1 +1,14 @@
-console.log("AI JavaScript connected");
+
+/*
+## API Links
+All data: [https://openapi.programming-hero.com/api/ai/tools](https://openapi.programming-hero.com/api/ai/tools)
+
+Single data details: https://openapi.programming-hero.com/api/ai/tool/${id}
+
+Single data Example: https://openapi.programming-hero.com/api/ai/tool/01
+ */
+ 
+ console.log("AI JavaScript connected");
+function handlefetch(){
+    fetch()
+}
